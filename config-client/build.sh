d1d655d2ef37735eb9f0860bc6afd45357edae69
allen <2505507040@qq.com>
2020-02-21 19:20:01 +0800

    update

--- config-client/build.sh
+++ config-client/build.sh
@@ -2,6 +2,6 @@
 
 mvn package
 
-docker build -t 192.168.100.249/study/config-client:latest .
+docker build -t 192.168.100.249/k8s/config-client:latest .
 
-docker push 192.168.100.249/study/config-client:latest
+docker push 192.168.100.249/k8s/config-client:latest
