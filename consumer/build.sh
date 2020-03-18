f27f96b7d70193d33467296f2644dcbfe56d2d5f
allen <2505507040@qq.com>
2020-03-18 21:28:03 +0800

    update

--- consumer/build.sh
+++ consumer/build.sh
@@ -2,6 +2,6 @@
 
 mvn package
 
-docker build -t 192.168.100.249/k8s/consumer:latest .
+docker build -t 192.168.1.100/k8s/consumer:latest .
 
-docker push 192.168.100.249/k8s/consumer:latest
+docker push 192.168.1.100/k8s/consumer:latest
--- producer/build.sh
+++ producer/build.sh
@@ -2,6 +2,6 @@
 
 mvn package
 
-docker build -t 192.168.100.249/k8s/producer:latest .
+docker build -t 192.168.1.100/k8s/producer:latest .
 
-docker push 192.168.100.249/k8s/producer:latest
+docker push 192.168.1.100/k8s/producer:latest
